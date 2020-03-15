de307552a63932943818dba0e33deca8ac11679c
Md. Yasir Aarafat <39982576+yasirarafat28@users.noreply.github.com>
2020-03-15 12:36:01 +0600

    fixes

--- resources/views/houseOwner/resident.blade.php
+++ resources/views/houseOwner/resident.blade.php
@@ -338,7 +338,7 @@
                                                     <div class="modal-dialog modal-lg" role="document">
                                                         <div class="modal-content">
                                                             <div class="modal-header">
-                                                                <h4 class="title" id="CreateModal">Preview of {{$item->name}}</h4>
+                                                                <h4 class="title" id="CreateModal">Preview of {{$item->resident->name}}</h4>
                                                             </div>
                                                             <div class="modal-body">
                                                                 <table class="table table-striped">
@@ -350,7 +350,7 @@
                                                                     </tr>
                                                                     <tr>
                                                                         <td>Resident Name</td>
-                                                                        <td>{{$item->$item->resident->name??'Unknown'}}</td>
+                                                                        <td>{{$item->resident->name??'Unknown'}}</td>
                                                                     </tr>
                                                                     <tr>
                                                                         <td>House_Area</td>
